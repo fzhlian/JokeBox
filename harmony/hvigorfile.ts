602eb406522e27baa2788b0ea0b11b2e1a559484
fzhlian <fzhlian@users.noreply.github.com>
2026-02-14 19:15:12 +0800

    auto-sync: 2026-02-14 19:15:11

--- harmony/hvigorfile.ts
+++ harmony/hvigorfile.ts
@@ -1,2 +1,2 @@
-﻿// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
 export { appTasks } from '@ohos/hvigor-ohos-plugin';

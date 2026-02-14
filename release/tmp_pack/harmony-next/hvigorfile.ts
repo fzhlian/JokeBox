bd9e5b430e1a90cee5b31fcab4a2af47dc8675eb
fzhlian <fzhlian@users.noreply.github.com>
2026-02-15 00:15:55 +0800

    auto-sync: 2026-02-15 00:15:54

---
+++ release/tmp_pack/harmony-next/hvigorfile.ts
@@ -0,0 +1,3 @@
+// Script for compiling build behavior.
+const { appTasks } = require('C:/Users/fzhlian/.hvigor/project_caches/2bf22346994211b5d65a78c952167251/workspace/node_modules/@ohos/hvigor-ohos-plugin');
+export { appTasks };
